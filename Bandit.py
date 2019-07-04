ab86c9ec7dfb453cb634fe39dfb4c3544e88a3c4
Niklas H <Typhoon.CommanderCool@gmail.com>
2019-07-05 00:23:00 +0200

    Final algorithm logic
    tbd: Plot correct figure aspect ration and use matplotlib PDF backend for LaTeX (requires MikTeX)

--- Bandit.py
+++ Bandit.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+from matplotlib import backend_bases
+from matplotlib.backends.backend_pgf import FigureCanvasPgf
+
 import numpy as np
 from tqdm import tqdm
 
@@ -8,22 +11,34 @@ EPS = [.1, .01, .009]
 
 
 def main():
-    eps = EPS[0]
-    result = np.zeros(1000)
+    opt = np.argmax(Q_STAR)
+    fig, (ax1, ax2) = plt.subplots(1, 2, tight_layout=True)
+    # https://matplotlib.org/users/pgf.html
+    # backend_bases.register_backend('pdf', FigureCanvasPgf)
+    ax1.set_ylabel('Average reward')
+    ax1.set_xlabel('Steps')
+    ax2.set_ylabel('% Optimal action')
+    ax2.set_xlabel('Steps')
 
-    for it_outer in tqdm(range(2000)):
-        Q = np.zeros(LEVER_NUM)
-        N = np.zeros(LEVER_NUM)
-        for it_inner in range(1000):
-            a = np.random.choice([optimize_reward(Q), explore()], p=[1 - eps, eps])
-            r = pull_lever(a)
-            N[a] += 1
-            Q[a] += (1 / N[a]) * (r - Q[a])
-            result[it_inner] += r
-
-    result /= 2000
-    result = np.cumsum(result) / np.arange(start=1, stop=1001)
-    plt.plot(result)
+    for eps in EPS:
+        result = np.zeros(1000)
+        bst_action = np.zeros(1000)
+        for it_outer in tqdm(range(2000)):
+            Q = np.zeros(LEVER_NUM)
+            N = np.zeros(LEVER_NUM)
+            for it_inner in range(1000):
+                a = np.random.choice([optimize_reward(Q), explore()], p=[1 - eps, eps])
+                r = pull_lever(a)
+                bst_action[it_inner] += (a == opt)
+                N[a] += 1
+                Q[a] += (1 / N[a]) * (r - Q[a])
+                result[it_inner] += r
+        result /= 2000
+        bst_action /= 2000
+        ax1.plot(result, label='ε=' + str(eps))
+        ax2.plot(bst_action, label='ε=' + str(eps))
+    ax1.legend(loc='lower right')
+    ax2.legend(loc='lower right')
     plt.show()
 
 
